6dfeb990c06a7d30b4cf2d014ac20d1a575512ef
Jagadeesh Kumar <pandarijagadheesh@gmail.com>
2020-03-11 09:53:02 +0530

    Initial Commit

---
+++ springsecurity/src/main/java/com/idm/config/SpringMVCWebAppInitializer.java
@@ -0,0 +1,23 @@
+package com.idm.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import com.idm.config.LoginApplicationConfig;
+
+public class SpringMVCWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { LoginApplicationConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
